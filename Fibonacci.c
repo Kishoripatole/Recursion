74dcc54b3daf941acd5dec415aaeb6f8f5a614a8
Kishoripatole <Kishoripatole2006@gmail.com>
2026-05-06 13:07:17 +0530

    famous sequence in math

---
+++ Fibonacci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int fibonacci(int n);
+
+int main()
+{
+     printf("Fibonacci of number:%d",fibonacci(8));
+     return 0;
+}
+
+int fibonacci(int n)
+{
+    int fib_Nm_1= fibonacci(n-1);
+    int fib_Nm_2= fibonacci(n-2);
+    int fibN=fib_Nm_1 + fib_Nm_2;
+}
+
